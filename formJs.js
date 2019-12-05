2ce66fe495f7c80c607e8808fa697e11ae85a34c
mercyu <55250413+mercyu@users.noreply.github.com>
2019-12-05 21:30:25 +0300

    11.1

--- formJs.js
+++ formJs.js
@@ -31,18 +31,3 @@ $('document').ready(function() { // после загрузки страницы
 		return false; // отключаем стандартную отправку формы
 	});
 });
-	/*
-document.addEventListener("DOMContentLoaded", function() { // событие загрузки страницы
-	history.pushState(null, null, 'https://sharedmean.github.io/televisions2');
-    // выбираем на странице все элементы типа textarea и input
-    document.querySelectorAll('textarea, input').forEach(function(e) {
-        // если данные значения уже записаны в sessionStorage, то вставляем их в поля формы
-        // путём этого мы как раз берём данные из памяти браузера, если страница была случайно перезагружена
-        e.value = window.sessionStorage.getItem(e.name, e.value);
-        // на событие ввода данных (включая вставку с помощью мыши) вешаем обработчик
-        e.addEventListener('input', function() {
-            // и записываем в sessionStorage данные, в качестве имени используя атрибут name поля элемента ввода
-            window.sessionStorage.setItem(e.name, e.value);
-        })
-    })
-	    }); */
